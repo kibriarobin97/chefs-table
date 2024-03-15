7c8554d54ae3b92cc1f5fcf3304990f2d73c8e53
kibriarobin97 <kibriarobin97@gmail.com>
2024-03-15 22:30:26 +0600

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='font-bold'>Chefs Table</h1>
+      <div>
+      <button className="btn btn-primary">Primary</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
